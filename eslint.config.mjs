59182e4c6790bc4a4e422657eef97e67599d3337
myroslava <murosja80@gmail.com>
2025-06-05 16:41:43 +0300

    hw_nodajs_02

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 
-export default [
-  pluginJs.configs.recommended,
+export default defineConfig([
   {
-    files: ['src/**/*.js'],
+    files: ['src/**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.node },
+    plugins: { js },
+    extends: ['js/recommended'],
     rules: {
       semi: 'error',
       'no-unused-vars': ['error', { args: 'none' }],
       'no-undef': 'error',
     },
   },
-];
+]);
