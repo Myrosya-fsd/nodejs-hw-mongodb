b33b7533aeeedabbda272b0a147c654050840378
myroslava <murosja80@gmail.com>
2025-06-13 16:30:18 +0300

    hw3-crud

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,19 +1,42 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  try {
-    const contacts = await ContactsCollection.find();
-    return contacts;
-  } catch (error) {
-    console.error(error.message);
-  }
+  const contacts = await ContactsCollection.find();
+  return contacts;
 };
 
 export const getContactById = async (contactId) => {
-  try {
-    const contact = await ContactsCollection.findById(contactId);
-    return contact;
-  } catch (error) {
-    console.error(error.message);
-  }
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (body) => {
+  const contact = await ContactsCollection.create(body);
+  return contact;
+};
+
+export const updateContact = async (contactId, body, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    body,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
 };
